b5e9f08a912f54508372acc1e0f6509659430c06
Robson <robbienroll@gmail.com>
2022-07-28 14:43:37 -0300

    Applying concepts from DevPro Class "Endereço de Banco de Dados". Hope it works!

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from decouple import config
 from pathlib import Path
+from functools import partial
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
