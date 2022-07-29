e9164fc2bddfc8cee7812d313689d29c41806389
Robson <robbienroll@gmail.com>
2022-07-28 21:33:44 -0300

    Applying concepts from DevPro Class "Endereço de Banco de Dados". Hope it works!

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,7 @@ from decouple import config
 from pathlib import Path
 from functools import partial
 import dj_database_url
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,8 +74,7 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-default_db_url = 'sqlite:///' + str(BASE_DIR) + '/' + 'db.sqlite3'
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
