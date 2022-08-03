6121cb5fe3e559136d141862b93aa1273af2debc
Robson <robbienroll@gmail.com>
2022-08-03 12:35:27 -0300

    APPLYING MODIFICATIONS MADE DURING DJANGO CLASS "Backup, Debug, Sentry e Conclusao"

--- pypro/settings.py
+++ pypro/settings.py
@@ -171,17 +171,17 @@ if AWS_ACCESS_KEY_ID:
 SENTRY_DSN = config('SENTRY_DSN', default=None)
 
 if SENTRY_DSN:
-    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration(),],
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration(), ],
 
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        # traces_sample_rate=1.0,
+                    # Set traces_sample_rate to 1.0 to capture 100%
+                    # of transactions for performance monitoring.
+                    # We recommend adjusting this value in production.
+                    # traces_sample_rate=1.0,
 
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        # send_default_pii=True
-    )
+                    # If you wish to associate users to errors (assuming you are using
+                    # django.contrib.auth) you may enable sending PII data.
+                    # send_default_pii=True
+                    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
