551998f00f0ffa35cfc0d55e8f1e088bce89ff7f
Robson <robbienroll@gmail.com>
2022-09-02 13:04:40 -0300

    Implementing Section "Relacionamento Muitos para Um"

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -15,7 +15,7 @@ def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
 
 
-def listar_aulas_de_modulos_ordenados(modulo:Modulo):
+def listar_aulas_de_modulos_ordenados(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
 
 
